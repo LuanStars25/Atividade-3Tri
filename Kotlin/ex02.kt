ad2508371c3e27ca2710d69f8eddba85348102b2
LuanZeichan <luan.russi.machado@escola.pr.gov.br>
2026-09-24 15:30:09 -0300

    Create ex02.kt

---
+++ Kotlin/ex02.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val nome = "Ana"
+    vall idade = 17
+    val cidade = "Paranaguá"
+    val linguagemFavorita = "Python"
+
+    printIn("APRESENTAÇÃO PESSOAL")
+    printIn("Nome: $nome")
+    printIn("idade: $idade anos")
+    printIn("Cidade: $cidade")
+    printIn("Linguagem favorita: $LinguagemFavorita")
+}
