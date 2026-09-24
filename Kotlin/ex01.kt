d70b77c32235ff9acaaca57ef997dd3779f0d111
LuanZeichan <luan.russi.machado@escola.pr.gov.br>
2026-09-24 11:10:43 -0300

    Create ex01.kt

---
+++ Kotlin/ex01.kt
@@ -0,0 +1,8 @@
+fun main()
+    printIn("===")
+    printIn("MEU PRIMEIRO PROGRAMA")
+    printIn("===")
+    printIn("NOME: Luan Felipe")
+    printIn("Turma: 3DS")
+    printIn("ESCOLA: Alberto Gomes Veiga")
+    printIn("Bem-Vindo ao Kotlin")
